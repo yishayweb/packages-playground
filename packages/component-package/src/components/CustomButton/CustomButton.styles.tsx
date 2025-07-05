f9226bd62b4bf6cd3ff4b0788c78252fab3012e5
yishayweb <yishayweb@gmail.com>
2025-07-05 15:05:38 +0300

    feat(component-package): updated green color

--- packages/component-package/src/components/CustomButton/CustomButton.styles.tsx
+++ packages/component-package/src/components/CustomButton/CustomButton.styles.tsx
@@ -5,7 +5,7 @@ export const StyledButton = styled(Button)({
   borderRadius: '4rem',
   fontWeight: 'bold',
   backgroundColor: '#00005f',
-  color: '#f7f7f7',
+  color: 'green',
   '&:disabled': {
     backgroundColor: '#b2b2b2',
     color: '#ffffff',
